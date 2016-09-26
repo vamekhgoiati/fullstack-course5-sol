fc3615f60976b227ce512eb2e34b63752cacd7f8
Vamekh Goiati <vamekh.goiati@gmail.com>
2016-09-26 07:42:24 +0400

    Module 2 assignment

---
+++ assignments/Mod2/app.js
@@ -0,0 +1,43 @@
+(function() {
+  'use strict';
+  angular.module("ShoppingListCheckOff", [])
+  .controller("ToBuyShoppingController", ToBuyShoppingController)
+  .controller("AlreadyBoughtShoppingController", AlreadyBoughtShoppingController)
+  .service("ShoppingListCheckOffService", ShoppingListCheckOffService);
+
+  ToBuyShoppingController.$inject = ['ShoppingListCheckOffService']
+  function ToBuyShoppingController(ShoppingListCheckOffService) {
+    var toBuyList = this;
+    toBuyList.items = ShoppingListCheckOffService.getToBuyList();
+
+    toBuyList.buyItem = function(itemIdx) {
+      ShoppingListCheckOffService.buy(itemIdx);
+    };
+  }
+
+  AlreadyBoughtShoppingController.$inject = ['ShoppingListCheckOffService']
+  function AlreadyBoughtShoppingController(ShoppingListCheckOffService) {
+    var boughtList = this;
+    boughtList.items = ShoppingListCheckOffService.getBoughtList();
+  }
+
+
+  function ShoppingListCheckOffService() {
+      var service = this;
+      var toBuyList = [ { name:'Cookies', quantity:10 }, { name:'Cookies', quantity:10 } ];
+      var boughtList = [];
+
+      service.buy = function(itemIdx) {
+        boughtList.push(toBuyList[itemIdx]);
+        toBuyList.splice(itemIdx, 1);
+      }
+
+      service.getToBuyList = function() {
+        return toBuyList;
+      }
+
+      service.getBoughtList = function() {
+        return boughtList;
+      }
+  };
+})()
